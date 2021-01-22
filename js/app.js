7020dd8768098827586081796fda6897f8ceabe7
James Crosslin <jamesmscrosslin@gmail.com>
2021-01-21 21:19:10 -0800

    added event listeners

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+let game;
+
+document.getElementById("btn__reset").addEventListener("click", (e) => {
+  document.getElementById("phrase").innerHTML = "";
+  [...document.getElementsByClassName("key")].forEach((key) => (key.className = "key"));
+  game = new Game();
+  game.startGame();
+});
+
+document.getElementById("qwerty").addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    game.handleInteraction();
+  }
+});
