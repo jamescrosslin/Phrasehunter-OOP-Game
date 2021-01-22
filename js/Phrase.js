dc7d80924f4746333a16d233bdb06dd511f3b8a1
James Crosslin <jamesmscrosslin@gmail.com>
2021-01-21 17:11:22 -0800

    created Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,30 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase;
+  }
+  addPhraseToDisplay() {
+    function makeListItem(letter) {
+      return `<li class="${letter === " " ? "space" : "hide letter " + letter}>${letter}"</li>`;
+    }
+
+    const html =
+      this.phrase.split("").reduce((phrase, letter) => {
+        return phrase + makeListItem(letter);
+      }, `<ul>`) + "</ul>";
+    console.log(html);
+    document.getElementById("phrase").innerHTML = html;
+  }
+  checkLetter(letter) {
+    return this.phrase.includes(letter);
+  }
+  showMatchedLetter() {
+    document.getElementsByClassName(letter).forEach((element) => {
+      element.classList.remove("hide");
+      element.classList.add("show");
+    });
+  }
+}
